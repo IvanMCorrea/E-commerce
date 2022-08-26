a671a5e0385356f4c26772fc85b22b7c0d5af41d
Ivan Correa <ivaanmc95@gmail.com>
2022-08-26 17:13:27 -0300

    v3.3: Corrijo error de img en carousel en celulares

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,10 +5,10 @@ import CarouselItem from "../carouselItem/CarouselItem";
 
 const Carousel = () => {
   const settings = {
-    className: "center carousel",
+    className: "carousel",
     centerMode: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     speed: 500,
     focusOnSelect: true,
     variableWidth: true,
@@ -19,22 +19,19 @@ const Carousel = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1,
         },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
         },
       },
     ],
